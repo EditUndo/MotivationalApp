18cfc453e9cd596313c7677da41057837e1f3211
mathew.j.sherry@gmail.com <mathew.j.sherry@gmail.com>
2020-05-02 05:36:13 +0200

    Formatting and Theme Changes Screen now randomizes colours on new quote Also formatted it so text takes up more of the screen Button is on the bottom of the screen and text for quotes are bigger Saved quotes screen also updates colour with main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'file_management.dart';
 
@@ -9,7 +11,7 @@ class MotivationApp extends StatelessWidget {
     return MaterialApp(
       title: 'Motivation',
       theme: ThemeData(
-        primaryColor: Colors.green,
+        primaryColor: Colors.black,
       ),
       home: Home(),
     );
@@ -22,9 +24,12 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  final _savedQuotes = Set<String>();  
+  final _savedQuotes = Set<String>();
   String _quoteToWrite = "";
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _biggerFont = const TextStyle(fontSize: 20.0);
+  final _smallerFont = const TextStyle(fontSize: 18.0);
+  Color _bgColor = Colors.lightBlue[600];
+  Color _buttonColor = Colors.deepOrange[300];
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +40,9 @@ class HomeState extends State<Home> {
           IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
         ],
       ),
+      backgroundColor: _bgColor,
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
           _buildQuote(),
           _buildButton(),
@@ -49,34 +55,42 @@ class HomeState extends State<Home> {
     //add favorite button
     bool alreadySaved = _savedQuotes.contains(_quoteToWrite);
 
-    return ListTile(
-      title: Text(_quoteToWrite),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
+    return Container(
+      height: 200.0,
+      child: ListTile(
+        title: Text(
+          _quoteToWrite,
+          style: _biggerFont,
+        ),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _savedQuotes.remove(_quoteToWrite);
+            } else {
+              _savedQuotes.add(_quoteToWrite);
+            }
+          });
+        },
       ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _savedQuotes.remove(_quoteToWrite);
-          } else {
-            _savedQuotes.add(_quoteToWrite);
-          }
-        });
-      },
     );
   }
 
   Widget _buildButton() {
     return Container(
+      padding: EdgeInsets.only(top: 50.0, bottom: 50.0),
+      alignment: Alignment.bottomCenter,
       child: RaisedButton(
         shape: RoundedRectangleBorder(
             borderRadius: new BorderRadius.circular(18.0),
             side: BorderSide(color: Colors.black12)),
         child: Text('Press For Quotes'),
         onPressed: _updateQuote,
-        textColor: Colors.white,
-        color: Colors.blueGrey,
+        textColor: Colors.black,
+        color: _buttonColor,
       ),
     );
   }
@@ -87,6 +101,30 @@ class HomeState extends State<Home> {
     _getRandomQuote().then((String text) {
       setState(() {
         _quoteToWrite = text;
+        Random random = Random();
+        switch (random.nextInt(4)) {
+          case 1: {
+            _bgColor = Colors.lightGreen[600]; _buttonColor = Colors.red[300];
+          }
+          break;
+
+          case 2: {
+            _bgColor = Colors.orange[400]; _buttonColor = Colors.blue[300];
+          }
+          break;
+
+          case 3: {
+            _bgColor = Colors.red[400]; _buttonColor = Colors.pink[300];
+          }
+          break;
+
+          case 0:
+          default: {
+            _bgColor = Colors.lightBlue[600]; _buttonColor = Colors.deepOrange[300];
+          }
+          break;
+
+        }
       });
     });
   }
@@ -95,6 +133,30 @@ class HomeState extends State<Home> {
     _getRandomQuote().then((String text) {
       setState(() {
         _quoteToWrite = text;
+        Random random = Random();
+        switch (random.nextInt(4)) {
+          case 1: {
+            _bgColor = Colors.lightGreen[600]; _buttonColor = Colors.red[300];
+          }
+          break;
+
+          case 2: {
+            _bgColor = Colors.orange[400]; _buttonColor = Colors.blue[300];
+          }
+          break;
+
+          case 3: {
+            _bgColor = Colors.red[400]; _buttonColor = Colors.pink[300];
+          }
+          break;
+
+          case 0:
+          default: {
+            _bgColor = Colors.lightBlue[600]; _buttonColor = Colors.deepOrange[300];
+          }
+          break;
+
+        }
       });
     });
   }
@@ -103,25 +165,34 @@ class HomeState extends State<Home> {
     return FileManagement.readRandomFileLine('assets/data/quotes.txt');
   }
 
-
   void _pushSaved() {
-        Navigator.of(context).push(
-          MaterialPageRoute<void>(
-            builder: (BuildContext context){
-              final Iterable<ListTile> tiles = _savedQuotes.map(
-                (String quote) {
-                  return ListTile(
-                    title: Text(
-                      quote,
-                      style:_biggerFont,
-                    ),
-                  );
-                },
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _savedQuotes.map(
+            (String quote) {
+              return ListTile(
+                title: Text(
+                  quote,
+                  style: _smallerFont,
+                ),
               );
-              final divided =ListTile.divideTiles(context: context, tiles: tiles,).toList();
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
 
-              return Scaffold(appBar: AppBar( title: Text('Saved Quotes'),), body: ListView(children: divided),);
-            },        ),);
-
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Quotes'),
+            ),
+            backgroundColor: _bgColor,
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
   }
 }
