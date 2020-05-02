e43857a4a44ddf4278ed9578c2c4fedd6175d663
mathew.j.sherry@gmail.com <mathew.j.sherry@gmail.com>
2020-05-02 18:20:28 +0200

    UI Changes to make everything prettier

--- lib/file_management.dart
+++ lib/file_management.dart
@@ -4,6 +4,9 @@ import 'dart:math';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 class FileManagement {
 
   static Future<String> readRandomFileLine(String fileName) async {
@@ -18,5 +21,15 @@ class FileManagement {
   static Future<String> readFile(String fileName) async {
     return await rootBundle.loadString(fileName);
   }
+
+/*
+  // Open the database and store the reference.
+  final Future<Database> database = openDatabase(
+    // Set the path to the database. Note: Using the `join` function from the
+    // `path` package is best practice to ensure the path is correctly
+    // constructed for each platform.
+    join(await getDatabasesPath(), 'doggie_database.db'),
+  );
+*/
 
 }
